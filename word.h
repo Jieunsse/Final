d7d34453a07e6ca0931a700576e300eeab8211bc
jieunsse <jieunsse@gmail.com>
2022-12-06 14:48:01 +0900

    word.h modified

--- word.h
+++ word.h
@@ -5,8 +5,33 @@
 using namespace std;
 
 
+class word {
+
+private:
+    int num;
+    string word;
+    string meaning;
+
+public:
+
+    void setWord(string word) {
+        this->word = word;
+    } 
+
+    string getWord() {
+        return word;
+    }
+
+    void setMeaning(string meaning) {
+        this->meaning = meaning;
+    }
+
+    string getMeaning() {
+        return meaning;
+    }
+};
 
 
+ 
 
-
-#endif WORD_H_
+#endif /* WORD_H_ */
